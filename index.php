1d9326f387b6b6db68b724521e469f7419a68fbc
Franco <fmaurizzio@alu.ing.unlp.edu.ar>
2020-11-25 18:17:49 -0300

    Agregado parcial de Formulario

--- index.php
+++ index.php
@@ -22,6 +22,29 @@
     </div>
 
     <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="p-3 m-2 bg-dark text-white">
+                    <h4>Formularios</h4>
+                    <form class="form-inline">
+                        <div class="form-group mx-sm-3 mb-2">
+                            <label for="inputPassword2" class="sr-only">Password</label>
+                            <input type="password" class="form-control" id="inputPassword2" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary mb-2">Confirm identity</button>
+                    </form>
+
+                </div>
+
+            </div>
+            <div class="col-6">
+                <div class="p-3 m-2 bg-success text-white">
+                    <h4>Formularios</h4>
+
+                </div>
+
+            </div>
+        </div>
 
         <div class="row">
             <div class="col-12">
